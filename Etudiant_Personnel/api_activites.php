728aa352001087ab6253f6ef9b4e4633348a0060
maimouna26 <maimounacissokho1@esp.sn>
2026-06-30 00:24:21 +0000

    Update api_activites.php

--- Etudiant_Personnel/api_activites.php
+++ Etudiant_Personnel/api_activites.php
@@ -9,23 +9,26 @@ require_once '../Gestionnaire/config/connexion.php';
 
 try {
     $pdo = connexionBD();
-
     $stmt = $pdo->query("
         SELECT 
-            id_act,
-            titre,
-            description,
-            type_act,
-            date_debut,
-            date_fin,
-            lieu,
+            a.id_act,
+            a.titre,
+            a.description,
+            a.type_act,
+            a.date_debut,
+            a.date_fin,
+            a.lieu,
+            s.nom_struct,
+            s.type_struct,
             CASE 
-                WHEN date_fin < NOW() THEN 'Terminé'
-                WHEN date_debut <= NOW() AND date_fin >= NOW() THEN 'En cours'
+                WHEN a.date_fin < NOW() THEN 'Terminé'
+                WHEN a.date_debut <= NOW() AND a.date_fin >= NOW() THEN 'En cours'
                 ELSE 'À venir'
             END AS statut
-        FROM ACTIVITE
-        ORDER BY date_debut ASC
+        FROM ACTIVITE a
+        LEFT JOIN GESTIONNAIRE g ON a.matricule_user = g.matricule_user
+        LEFT JOIN STRUCTURE s ON g.id_struct = s.id_struct
+        ORDER BY a.date_debut ASC
     ");
     $activites = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($activites);
